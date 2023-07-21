62246bb9c9a32118408b61c0cb909e4f64fad221
Fabio Lucio <fabiolucio277@gmail.com>
2023-07-21 16:04:53 -0300

    get dos produtos painel administrativo e utilizando delete dos produtos

--- src/Screens/AdministrativePanel/index.tsx
+++ src/Screens/AdministrativePanel/index.tsx
@@ -3,29 +3,37 @@ import { useNavigate } from 'react-router-dom';
 import { AuthContext, AuthContextData } from '../../contexts/Auth';
 import { NavBar } from '../../components/Navbar';
 import { Footer } from '../../components/Footer';
-import { adminpanelproductapi } from '../../services/api'; // Importe a API correta para produtos
+import { adminpanelproductapi } from '../../services/api';
 import {
   ContainerHome,
   ContainerFormLogin,
   ContainerButtons,
   ContainerHomeform,
   ContainerTitulo,
-  ContainerSubTitulo
+  ContainerSubTitulo,
+  ProductContainer,
+  ProductHeader,
+  ProductItem,
+  ProductInfo,
+  ProductActions,
+  DeleteButton,
+  EditButton,
 } from './style';
 
 type Product = {
   id: number;
   name: string;
-  // Adicione outras propriedades do produto aqui
+  price: number;
+  brand: string;
 };
 
-export const AdministrativePanel = () => {
-  const { userAdminPanel } = useContext<AuthContextData>(AuthContext);
-  const navigate = useNavigate();
+// ...imports
 
-  
+export const AdministrativePanel = () => {
+  // ...rest of the code
 
   const [products, setProducts] = useState<Product[]>([]);
+  const [showProductItems, setShowProductItems] = useState(false);
 
   useEffect(() => {
     const cachedValues = localStorage.getItem('@adminPanelValues');
@@ -35,19 +43,37 @@ export const AdministrativePanel = () => {
     }
   }, []);
 
-  // Função para carregar os produtos
   const loadProducts = async () => {
     try {
       const response = await adminpanelproductapi.get('/');
-      setProducts(response.data);
+      const productsData: Product[] = response.data.map((product: any) => ({
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        brand: product.brand,
+      }));
+      setProducts(productsData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  const handleDeleteProduct = async (productId: number) => {
+    try {
+      await adminpanelproductapi.delete(`delete/${productId}`);
+      setProducts((prevProducts) => prevProducts.filter((product) => product.id !== productId));
     } catch (error) {
       console.error(error);
     }
   };
 
   const handleProductClick = () => {
-    // Adicione a lógica para tratar o clique no botão "Produto"
-    loadProducts(); // Carregar os produtos ao clicar no botão
+    loadProducts();
+    setShowProductItems(!showProductItems);
   };
 
   return (
@@ -56,27 +82,42 @@ export const AdministrativePanel = () => {
       <ContainerHome>
         <ContainerHomeform>
           <ContainerFormLogin>
-          <ContainerTitulo>Painel Administrativo</ContainerTitulo>
-          <ContainerSubTitulo>Bem vindo, admin!</ContainerSubTitulo>
-        
+            <ContainerTitulo>Painel Administrativo</ContainerTitulo>
+            <ContainerSubTitulo>Bem-vindo, admin!</ContainerSubTitulo>
+
             <ContainerButtons>
               <button type="button" onClick={handleProductClick}>Produto</button>
               <button type="button">Usuário</button>
               <button type="button">Pedidos</button>
               <button type="button">Adicionar Novo</button>
             </ContainerButtons>
-            {/* Renderize a lista de produtos aqui */}
-            <ul>
-              {products.map((product) => (
-                <li key={product.id}>{product.name}</li>
+
+            <ProductContainer>
+              <ProductHeader>
+                <p style={{ flexBasis: '25%' }}>Nome</p>
+                <p style={{ flexBasis: '25%' }}>Preço</p>
+                <p style={{ flexBasis: '25%' }}>Marca</p>
+                <p style={{ flexBasis: '25%' }}>Ações</p>
+              </ProductHeader>
+
+              {showProductItems && products.map((product) => (
+                <ProductItem key={product.id}>
+                  <ProductInfo>
+                    <p style={{ flexBasis: '25%' }}>{product.name}</p>
+                    <p style={{ flexBasis: '25%' }}>{product.price}</p>
+                    <p style={{ flexBasis: '25%' }}>{product.brand}</p>
+                  </ProductInfo>
+                  <ProductActions>
+                    <EditButton type="button">Editar</EditButton>
+                    <DeleteButton type="button" onClick={() => handleDeleteProduct(product.id)}>Excluir</DeleteButton>
+                  </ProductActions>
+                </ProductItem>
               ))}
-            </ul>
+            </ProductContainer>
           </ContainerFormLogin>
-          {/* Resto do código */}
         </ContainerHomeform>
       </ContainerHome>
       <Footer />
     </>
   );
 };
-
--- src/Screens/AdministrativePanel/style.ts
+++ src/Screens/AdministrativePanel/style.ts
@@ -1,36 +1,29 @@
 import styled from 'styled-components';
 import BgBody from '../../assets/img/bg01.jpg';
 
-// Estilização do container principal da página
-
 export const ContainerHome = styled.div`
   background: var(--background-primary, #EFF2F6);
-  min-height: 100vh; // Alteração: altura mínima para ocupar pelo menos 100% da altura da tela
+  min-height: 100vh;
   width: 100%;
   display: flex;
   align-items: center;
-  flex-direction: column;
   justify-content: center;
-  background-image: url(${BgBody});    
+  background-image: url(${BgBody});
 `;
 
-// Estilização do container que envolve o formulário
-
 export const ContainerHomeform = styled.div`
-  background:  #fff 0.38;
+ 
   border: 1px solid;
-  width: 1150px;
-  height: 900px;
+  width: 90%;
+  max-width: 1150px;
+  height: auto;
   flex-shrink: 0;
   border-radius: 10px;
   padding-top: 30px;
   box-shadow: 0px 0px 28px 0px rgba(0,0,0,0.38);
   -webkit-box-shadow: 0px 0px 28px 0px rgba(0,0,0,0.38);
-  -moz-box-shadow: 0px 0px 28px 0px rgba(0,0,0,0.38);  
-  
+  -moz-box-shadow: 0px 0px 28px 0px rgba(0,0,0,0.38);
 `;
-
-// Estilização do container do formulário
 
 export const ContainerFormLogin = styled.div`
   color: #fff;
@@ -41,73 +34,24 @@ export const ContainerFormLogin = styled.div`
   justify-content: center;
 `;
 
-
-// Estilização do formulário
-
-export const FormLogin = styled.form`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
-
-// Estilização do container dos inputs
-
-export const ContainerInputs = styled.div`
-  margin: 1.375rem 0;
-  width: 455px;
-  height: 40px;
-  flex-shrink: 0;
-  padding: 0 15px;
-
-  label {
-    font-weight: 700;
-    font-size: 1rem;
-    line-height: 1.1875rem;
-    color: #fff;
-  }
-
-  input {
-    width: 100%;
-    margin-top: 0.4375rem;
-    background-color: transparent;
-    border: 1px solid var(--blue-300);
-    padding: 0.5rem 0.75rem;
-    font-weight: 700; 
-    border-radius: 6px;
-    color: #fff;
-
-    &::placeholder {
-      color: var(--blue-200);
-      font-weight: 200;
-    }
-
-
-  }
-`;
-
-
-
-// Estilização do container dos botões
-
 export const ContainerButtons = styled.div`
- display: flex;
-  justify-content: center; /* Alteração para alinhar os botões horizontalmente */
-  align-items: center; /* Alinhar os botões verticalmente */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
 
   button {
     cursor: pointer;
-   
     border-radius: 5px;
     font-weight: 700;
     font-size: 1rem;
     line-height: 1.1875rem;
     color: blue;
-    border: 10px solid blue;
+    border: 2px solid blue;
     padding: 8px 68px;
-    margin-right: 10px; /* Adição de margem entre os botões para separá-los */
-    white-space: nowrap; /* Impedir que o texto quebre em várias linhas */
+    margin: 0 10px;
+    white-space: nowrap;
+    transition: opacity 0.2s;
 
     &:not(:disabled):hover {
       opacity: 0.9;
@@ -115,65 +59,94 @@ export const ContainerButtons = styled.div`
   }
 `;
 
-// Estilização do container do botão "Voltar"
+export const ProductContainer = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  max-width: 1100px;
+ 
+`;
 
-export const ContainerButtonscreate = styled.div`
+export const ProductHeader = styled.div`
+ display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  padding: 10px 0;
+  
+  text-align: center;
+  font-weight: 700;
+  margin-left: 50px; /* Adiciona margem esquerda para afastar os itens do cabeçalho */
+`;
+
+export const ProductItem = styled.li`
   display: flex;
-  justify-content: center;
-  color: var(--blue-200);
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 16px;
+  border-bottom: 1px solid #ddd;
+`;
 
-  span {
-    font-weight: 700;
+export const ProductInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex: 1;
+
+  p {
+    flex-basis: 33.33%;
+    text-align: center;
   }
+`;
 
-  a { 
-    margin-top: 80px;
-    cursor: pointer;
-  }
-`;  
+export const ProductActions = styled.div`
+  display: flex;
+`;
 
-export const RectangularButton = styled.button`
+export const EditButton = styled.button`
   cursor: pointer;
-  background: #000;
+  background: blue;
   border-radius: 5px;
   font-weight: 700;
   font-size: 1rem;
   line-height: 1.1875rem;
-  color: blue;
-  border: 10px solid blue;
-  padding: 8px 68px;
-
-  &:not(:disabled):hover {
-    opacity: 0.9;
-  }
+  color: white;
+  border: 2px solid blue;
+  padding: 8px 16px;
+  margin-right: 10px;
 `;
 
+export const DeleteButton = styled.button`
+  cursor: pointer;
+  background: red;
+  border-radius: 5px;
+  font-weight: 700;
+  font-size: 1rem;
+  line-height: 1.1875rem;
+  color: white;
+  border: 2px solid red;
+  padding: 8px 16px;
+`;
 
-export const ContainerTitulo = styled.div`
+export const ContainerTitulo = styled.h1`
   font-family: 'Public Sans', sans-serif;
   font-size: 36px;
   font-weight: 600;
   line-height: 44px;
   letter-spacing: -1.5px;
-  text-align: left;
+  text-align: center;
   color: #fff;
   margin-bottom: 20px;
 `;
 
-export const ContainerSubTitulo = styled.div`
+export const ContainerSubTitulo = styled.p`
   font-family: 'Public Sans', sans-serif;
   font-size: 16px;
   font-weight: 500;
   line-height: 26px;
   letter-spacing: -0.2px;
-  text-align: left;
+  text-align: center;
   color: #fff;
   margin-bottom: 40px;
 `;
-
-
-
-
-
-
-
